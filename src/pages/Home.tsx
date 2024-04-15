0c3cf0090a6bec5bbcd942db1d67dfa7af45af55
Akash <therealakash13>
2024-04-15 16:37:18 +0530

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Spinner from '../components/Spinner';
+import { HomePageVideos } from '../Types';
+import Card from '../components/Card';
+import { clearVideos } from '../store';
+
+export default function Home() {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearVideos());
+    };
+  },[dispatch]);
+  
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+    console.log(videos);
+  },[dispatch]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh"}}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={900}
+            style={{ width: '1600px' }}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
+}
